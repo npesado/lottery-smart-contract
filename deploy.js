614fb4d7f295d496ad04c0e177ec1f5adbad8bb3
Nazareno Pesado <nano@bitopia.digital>
2022-05-03 13:23:01 +0000

    feat: initial commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'clock still cushion security wire water dance slab valid ship hammer chalk', // Your mnemonic
+    'https://rinkeby.infura.io/v3/84aafbf69a0c444790c3fdd16c8455cf' // Your infura url
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+deploy();
